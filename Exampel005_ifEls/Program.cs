6ceb54ea90fe5b69d6d7bc1eda48a61a1705bbf5
Marsel <marsel5555@yandex.ru>
2023-02-10 16:19:19 +0300

    Exampel005_ifEls

---
+++ Exampel005_ifEls/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "марсель")
+{
+    Console.WriteLine("Ура, это же Марсель!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
